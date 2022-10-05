bfd242dbdd3c9f2f29370641d9b59251ab0af075
Timilehin Arigbede <harriemannie@gmail.com>
2022-10-05 10:46:12 +0100

    Posts Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,13 @@ import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
 const IndexPage = ({ data }) => {
-  console.log(data)
+  const {
+    allMdx: { nodes: posts },
+  } = data
   return (
     <Layout>
       <Hero showPerson />
+      <Posts posts={posts} title="recently published" />
     </Layout>
   )
 }
