0ca534b7d78feac04f6500f9863ac3285309a57f
Timilehin Arigbede <harriemannie@gmail.com>
2022-10-05 13:23:04 +0100

    Categories Query

--- src/templates/category-template.js
+++ src/templates/category-template.js
@@ -4,14 +4,36 @@ import Hero from '../components/Hero'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
 
-const CategoryTemplate = ()=> {
-  
-  return (
-   <h2>category template</h2>
-  )
+const CategoryTemplate = props => {
+  console.log(props)
+  return <Layout>category template</Layout>
 }
 
-
-
+export const query = graphql`
+  query GetCategories($category: String) {
+    allMdx(
+      sort: { fields: frontmatter___date, order: DESC }
+      filter: { frontmatter: { category: { eq: $category } } }
+    ) {
+      nodes {
+        id
+        excerpt
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          slug
+          date(formatString: "MMMM, DD, YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default CategoryTemplate
